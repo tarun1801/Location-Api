fc07355c7a0834f603a364a586783b763e9020f2
tarun1801 <55619064+tarun1801@users.noreply.github.com>
2021-07-27 10:09:54 +0530

    Refactoring Done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const axios = require('axios')
 let ar = [];
-
+let result = {};
 
 app.get('/location',(req,res)=>{
     
@@ -12,46 +12,69 @@ app.get('/location',(req,res)=>{
     const long1 = req.query.long1 ;
     const lat2 = req.query.lat2;
     const long2 = req.query.long2 ;
-    let result = {};
-    // result.lat1 = lat1;
-    // result.long1 = long1 ;
-    // result.lat2 = lat2 ;
-    // result.long2 = long2 ; 
-    console.log(lat1,long1 ,lat2 , long2)
+    
     axios({
         method : "GET",
         url:`https://api.mapbox.com/directions/v5/mapbox/driving/${long1},${lat1};${long2},${lat2}?steps=true&voice_instructions=true&banner_instructions=true&voice_units=imperial&waypoint_names=Home;Work&access_token=pk.eyJ1IjoidGFydW4tMTgwMSIsImEiOiJja3FpdzZzdXcwMWNuMnFwaGZucHZqMmxwIn0.YSYoTUJkW6ppiRO16-RxIA`,
        
     }).then(response  => {
 
-        result.from = {latitude :lat1 ,longitude : long1};
-        result.to = {latitude :lat2 ,longitude : long2};
-        result.duration = response.data.routes[0].duration+" s"
-        result.distance = response.data.routes[0].distance+" m"
-
-        let directions =  response.data.routes[0].legs[0].steps
-        //console.log("Directions from response",directions)
-
-        for(var i = 0 ; i < directions.length ; i++)
-        {
-            ar.push(directions[i].voiceInstructions)
-        }
-        //console.log("Direction array",ar)
-        result.directions = ar;
+        setOrigin(lat1,long1);
+        setDestination(lat2,long2);
+        setDuration(response);
+        setDistance(response);
+        setDirections(response);
+        setGoogleMap(lat1,long1,lat2,long2);
         res.json(result)
      console.log(response.data)
     }).catch(e =>{
         console.log("Error",e)
            
     })
-
-   
-     
-  
-
 })
 
 
+function setOrigin(lat1,long1){
+    return result.from = {latitude :lat1 ,longitude : long1};
+
+}
+function setDestination(lat2,long2)
+{
+    result.to = {latitude :lat2 ,longitude : long2};
+}
+function setDuration(response)
+{
+    let s = parseInt(response.data.routes[0].duration);
+    let min = Math.floor(s/60);
+    let hrs =Math.floor(min/60);
+     
+    result.duration = hrs + " H "+ min+ " min "
+}
+function setDistance(response){
+    result.distance = parseInt(response.data.routes[0].distance)/1000+" km"
+}
+function setDirections(response){
+    
+    let directions =  response.data.routes[0].legs[0].steps
+ 
+
+    for(var i = 0 ; i < directions.length ; i++)
+    {
+        ar.push(directions[i].voiceInstructions)
+    }
+  
+   
+    result.directions = ar;
+
+}
+function setGoogleMap(lat1,long1,lat2,long2)
+{
+    result.gDirections = `https://www.google.com/maps/dir/${lat1},${long1}/${lat2},${long2}/`
+}
+function fetchData(lat1,long1,lat1,long2)
+{
+    
+}
 
 
 
